cf0eaae6090d1d9547f22619793182be1fcf7f49
VestaKyra <anastasya1414dmytrivna@gmail.com>
2024-03-07 00:36:38 +0200

    First push, only layouts

---
+++ app/src/main/java/com/example/littleadventure/TitleActivity.kt
@@ -0,0 +1,20 @@
+package com.example.littleadventure
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TitleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_title)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
